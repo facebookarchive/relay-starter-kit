586e0156e593054fb284357d4a318eb2d1e94467
cpojer <christoph.pojer@gmail.com>
2015-08-18 01:09:41 -0700

    Add react-router example.

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ var app = new WebpackDevServer(compiler, {
 });
 // Serve static resources
 app.use('/', express.static('public'));
+app.use('/widget/:id', express.static('public'));
 app.use('/node_modules/react', express.static('node_modules/react'));
 app.use('/node_modules/react-relay', express.static('node_modules/react-relay'));
 app.listen(APP_PORT, () => {
