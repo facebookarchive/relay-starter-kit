e67267ffd65cb2d39dbc0f26c30ad63ffe53ce0b
h1nr1ch <hinrich.sager+github@gmail.com>
2015-08-29 23:35:33 +0200

    Update server.js
    Fix confused ports. Set APP_PORT to 8080 and GRAPHQL_PORT to 3000. Not a bug but breaks "Principle of Least Surprise" ;)

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ import webpack from 'webpack';
 import WebpackDevServer from 'webpack-dev-server';
 import {Schema} from './data/schema';
 
-const APP_PORT = 3000;
-const GRAPHQL_PORT = 8080;
+const APP_PORT = 8080;
+const GRAPHQL_PORT = 3000;
 
 // Expose a GraphQL endpoint
 var graphQLServer = express();
