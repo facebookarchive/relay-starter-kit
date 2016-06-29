ad699d972a3cafb09d3aedd0a1b83dd3b0b62cf2
Daniel Beck <nowseemee@gmail.com>
2016-06-29 22:11:58 +0400

    add scroll behavior

--- js/components/WidgetList.js
+++ js/components/WidgetList.js
@@ -1,19 +1,63 @@
 import React from 'react';
-import Widget from './Widget';
+import Widget from './Widget/index';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 
-const WidgetList = (props) => (
-  <div className="rsk-widget-list">
-    <ul>
-      {props.viewer.widgets.edges.map(edge => (
-        <Widget
-          {...props}
-          key={edge.node.id}
-          widget={edge.node}
-        />
-      ))}
-    </ul>
-  </div>
-);
+
+class WidgetList extends React.Component {
+  scrollToBottom() {
+    this.refs.rskWidgetList.scrollTop = this.refs.rskWidgetList.scrollHeight;
+  }
+
+  componentDidMount() {
+    this.refs.rskWidgetList.scrollTop = this.refs.rskWidgetList.scrollHeight
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.viewer.widgetsCount > this.props.viewer.widgetsCount) {
+      setTimeout(() => {
+        this.refs.rskWidgetList.scrollTop = this.refs.rskWidgetList.scrollHeight
+      }, 300)
+    }
+  }
+
+  handleScroll(event) {
+    if (!event.target.scrollTop && this.props.viewer.widgetsCount > this.props.viewer.widgets.edges.length) {
+      let loadingIndicator = this.refs.loadingIndicator;
+      let rskWidgetListUl = this.refs.rskWidgetListUl;
+      const firstElement = rskWidgetListUl.children[0].children[0];
+      loadingIndicator.className = 'loading-indicator';
+      rskWidgetListUl.className = 'feather';
+
+      this.props.loadMoreWidgets().then(() => {
+        setTimeout(()=>{
+          loadingIndicator.className = 'loading-indicator hidden';
+          rskWidgetListUl.className = '';
+          firstElement.scrollIntoView();
+        }, 600);
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div ref="rskWidgetList" onScroll={::this.handleScroll} className="rsk-widget-list">
+        <div ref="loadingIndicator" className="loading-indicator hidden"><i className="fa fa-refresh fa-spin" /></div>
+        <ul ref="rskWidgetListUl">
+          <ReactCSSTransitionGroup transitionName="expand" transitionEnterTimeout={0} transitionLeaveTimeout={0}>
+            {this.props.viewer.widgets.edges.map(edge => (
+              <Widget
+                {...this.props}
+                key={edge.node.id}
+                widget={edge.node}
+              />
+            ))}
+          </ReactCSSTransitionGroup>
+        </ul>
+      </div>
+    );
+  }
+}
+
 
 
 export default WidgetList;
