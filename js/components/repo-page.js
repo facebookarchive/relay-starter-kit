46750d82ac8f037191b9560262c7553c6cb18892
oliverxchen <oliverxchen@gmail.com>
2016-09-21 16:13:25 +0800

    top navbar

--- js/components/repo-page.js
+++ js/components/repo-page.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import TopBar from './navigation/top-bar'
 import Grid from 'react-bootstrap/lib/Grid'
 import Col from 'react-bootstrap/lib/Col'
 import MetadataTabs from './metadata/metadata-tabs'
@@ -7,8 +8,9 @@ import Button from 'react-bootstrap/lib/Button'
 export default class RepoPage extends React.Component {
   render() {
     return <Grid>
-      <Col lg={10} md={11}><MetadataTabs/></Col><Col lg={2} md={1}>
-      <Button bsStyle="primary">Primary</Button></Col>
-    </Grid>
+        <TopBar/>
+        <Col lg={10} md={11}><MetadataTabs/></Col><Col lg={2} md={1}>
+        <Button bsStyle="primary">Primary</Button></Col>
+      </Grid>
   }
 }
