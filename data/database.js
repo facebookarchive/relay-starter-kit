10c7d7ed5906282ba771469a156192c3fd8e77c3
Alain Armand <aarmand.inbox@gmail.com>
2017-01-17 12:44:10 +0100

    cleaned up db

--- data/database.js
+++ data/database.js
@@ -1,34 +1,12 @@
-// const Relay = require('react-relay')
 
-class User {}
-// class Quote {}
-// const quote = new Quote()
+class User { }
 
-// Mock authenticated ID
-// const VIEWER_ID = 'me'
 const viewer = new User();
 viewer.id = '1';
 viewer.name = 'Anonymous';
 
-// Mock user data
-// const viewer = new User()
-// viewer.id = VIEWER_ID
-
-// const usersById = {
-//     [VIEWER_ID]: viewer,
-// }
-
-// function getUser(id) {
-//     return usersById[id]
-// }
-
-// function getViewer() {
-//     return getUser(VIEWER_ID)
-// }
-
 module.exports = {
-    User,
-    // Export methods that your schema can use to interact with your database
+  User,
   getUser: (id) => id === viewer.id ? viewer : null,
   getViewer: () => viewer,
 }
