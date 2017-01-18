2f52954d0a104ac6d963df30daf2051cf999ff2e
Alain Armand <aarmand.inbox@gmail.com>
2017-01-18 10:18:08 +0100

    debug screenshot added

--- js/components/App.js
+++ js/components/App.js
@@ -14,6 +14,7 @@ class App extends React.Component {
   }
 
   render() {
+    console.log(JSON.stringify(this.props, null, 4))
     return (
       <div className="quotes-library">
         <SearchForm searchAction={this.search} />
